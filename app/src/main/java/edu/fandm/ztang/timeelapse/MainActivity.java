25dbd8c0a4fa2a4543acb3f80d5b828641597a75
xwu1 <xwu1@fandm.edu>
2017-04-03 18:54:42 -0400

    WRITE_EXTERNAL_STORAGE

--- app/src/main/java/edu/fandm/ztang/timeelapse/MainActivity.java
+++ app/src/main/java/edu/fandm/ztang/timeelapse/MainActivity.java
@@ -8,7 +8,6 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
-import android.os.Handler;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -18,13 +17,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -42,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
         //Check whether got the permissions
         int permission = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.RECORD_AUDIO);
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
         if (permission != PackageManager.PERMISSION_GRANTED) {
             Log.i(TAG, "Permission to record denied");
             //Request runtime permissions
