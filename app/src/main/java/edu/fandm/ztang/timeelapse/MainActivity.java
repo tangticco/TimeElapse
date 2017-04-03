f902dee60bd605cb2eab76c4245af188a0828679
xwu1 <xwu1@fandm.edu>
2017-04-03 17:06:14 -0400

    1 photo only

--- app/src/main/java/edu/fandm/ztang/timeelapse/MainActivity.java
+++ app/src/main/java/edu/fandm/ztang/timeelapse/MainActivity.java
@@ -1,30 +1,66 @@
 package edu.fandm.ztang.timeelapse;
 
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
+import android.os.Handler;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    private String TAG = "TimeElapse";
+
     private ImageView mImageView;
+    private String mCurrentPhotoPath;
+    private int PERMS_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Check whether got the permissions
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECORD_AUDIO);
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "Permission to record denied");
+            //Request runtime permissions
+            String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+            ActivityCompat.requestPermissions(this, permissions, PERMS_REQUEST_CODE);
+
+        }
+
+        //Create a folder for this app
+        String state = Environment.getExternalStorageState();
+        if(Environment.MEDIA_MOUNTED.equals(state)) {
+            // Get the directory for storage
+            File Root = Environment.getExternalStorageDirectory();
+            File Dir = new File(Root.getAbsolutePath() + "/TimeElapse");
+            if (!Dir.exists()) { // Create one if there is not
+                Dir.mkdirs();
+            }
+        }
     }
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
@@ -39,48 +75,66 @@ public class MainActivity extends AppCompatActivity {
         // Ensure that there's a camera activity to handle the intent
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             // Create the File where the photo should go
-            File photoFile = null;
+            File photoFolder = null;
             try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
+                photoFolder = createImageFolder();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
+            int counter = 1;
+
+            Log.d(TAG, photoFolder.getAbsolutePath());
+
+            for(int i=1; i<2; i++){
+                File photoFile = new File(photoFolder, counter + ".jpg");
+                photoFile.mkdirs();
+                counter++;
                 Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.example.android.fileprovider",
+                        "edu.fandm.android.fileprovider",
                         photoFile);
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
                 startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
+
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        mImageView = (ImageView) findViewById(R.id.view);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            mImageView.setImageBitmap(imageBitmap);
-        }
-    }
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        mImageView = (ImageView) findViewById(R.id.view);
+//        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+//            Bundle extras = data.getExtras();
+//            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//            mImageView.setImageBitmap(imageBitmap);
+//        }
+//    }
 
-    String mCurrentPhotoPath;
 
-    private File createImageFile() throws IOException {
-        // Create an image file name
+
+    private File createImageFolder() throws IOException {
+        String state = Environment.getExternalStorageState();
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
+        // Get the directory for storage
+        File Root = Environment.getExternalStorageDirectory();
+
+        Log.d(TAG, Root.getAbsolutePath());
+
+        File Dir = new File(Root.getAbsolutePath()+"/TimeElapse");
+        if(!Dir.exists()){ // Create one if there is not
+            Dir.mkdirs();
+        }
+        File folder = new File(Dir, "TE" + timeStamp);
+        folder.mkdirs();
+
 
         // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = image.getAbsolutePath();
-        return image;
+        //mCurrentPhotoPath = image.getAbsolutePath();
+        return folder;
     }
 
     private void galleryAddPic() {
